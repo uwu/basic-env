23089c3a05e0409564338e947d578d8079066119
Phorcys <57866459+phorcys420@users.noreply.github.com>
2024-01-23 10:48:27 +0100

    feat: bump base image version to v0.6

--- main.tf
+++ main.tf
@@ -270,7 +270,7 @@ resource "docker_image" "base_image" {
   build {
     context = "./docker/base"
 
-    tag     = ["uwunet/basic-env-base", "uwunet/basic-env-base:latest", "uwunet/basic-env-base:v0.5"]
+    tag     = ["uwunet/basic-env-base", "uwunet/basic-env-base:latest", "uwunet/basic-env-base:v0.6"]
   }
 
   keep_locally = true
